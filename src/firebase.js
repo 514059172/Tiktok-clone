4f54f29bcf88a16119757990edba409445605d49
bilder <514059172@163.com>
2021-03-04 12:41:56 +0800

    init

---
+++ src/firebase.js
@@ -0,0 +1,10 @@
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+  apiKey: 'AIzaSyBLTPfRmIBZPm6rQ9CJ229pu-Qxu3Wd2tw',
+  authDomain: 'tiktok-clone-c130b.firebaseapp.com',
+  projectId: 'tiktok-clone-c130b',
+  storageBucket: 'tiktok-clone-c130b.appspot.com',
+  messagingSenderId: '272497456656',
+  appId: '1:272497456656:web:895aff7371b25a34bc7c95',
+  measurementId: 'G-PH3TXYCG6P',
+};
